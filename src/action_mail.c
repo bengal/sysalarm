ba1c004533f0b93ac54a94d95ba19ab52fef7c5e
bengal <b.galvani@gmail.com>
2011-03-06 10:44:38 +0100

    use libesmtp

--- src/action_mail.c
+++ src/action_mail.c
@@ -9,6 +9,9 @@
 #include <unistd.h>
 #include <time.h>
 
+#include <libesmtp.h>
+#include <auth-client.h>
+
 #include "base.h"
 #include "util.h"
 
@@ -37,7 +40,7 @@ static void prepare_mail_message(struct mail_action_config *config,
 	strftime(timestr, 256, "%c", timeinfo);
 
 	snprintf(buffer, size,
-			"From: %s\nTo: %s\nSubject: %sDate: %s\n\n"
+			"From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n"
 			"An alarm condition was detected:\n%s",
 			config->mail_to, config->mail_from, config->mail_subject,
 			timestr, result_get_description(cond_res));
@@ -46,42 +49,50 @@ static void prepare_mail_message(struct mail_action_config *config,
 static void send_mail_smtp(struct mail_action_config *config, struct result *cond_res,
 		struct result *result)
 {
-	int sockfd;
-	FILE *sock;
-	char hostname[256];
-	char buffer[MAIL_MSG_MAX_SIZE];
 
-	if((sockfd = connect_tcp(config->smtp_server, 25)) == -1){
-		result->code = ACTION_ERROR;
-		snprintf(result->desc, RESULT_DESC_LEN, "Could not connecto to %s",
-				config->smtp_server);
-		return;
+	smtp_session_t session;
+	smtp_message_t message;
+	smtp_recipient_t recipient;
+	auth_context_t authctx;
+	char body[MAIL_MSG_MAX_SIZE];
+	const smtp_status_t *status;
+	char server[1024];
+
+	/* WiP */
+
+	//prepare_mail_message(config, buffer, MAIL_MSG_MAX_SIZE, cond_res);
+	snprintf(body, MAIL_MSG_MAX_SIZE, "An alarm condition was detected:\n%s",
+			result_get_description(cond_res));
+
+	auth_client_init ();
+	session = smtp_create_session ();
+	message = smtp_add_message (session);
+
+	smtp_set_header (message, "To", NULL, config->mail_to);
+	snprintf(server, 1024, "%s:%d", config->smtp_server, 25);
+	smtp_set_server (session, server);
+	smtp_add_recipient(message, config->mail_to);
+	smtp_set_message_str(message, body);
+
+	if (!smtp_start_session(session)) {
+		char buf[128];
+		//fprintf(stderr, "SMTP server problem %s\n", smtp_strerror(smtp_errno(), buf, sizeof buf));
+		snprintf(result->desc, RESULT_DESC_LEN, "SMTP server problem: %s",
+				smtp_strerror(smtp_errno(), buf, sizeof(buf)));
+
+	} else {
+		/* Report on the success or otherwise of the mail transfer.
+		 */
+		status = smtp_message_transfer_status(message);
+		printf("%d %s", status->code, (status->text != NULL) ? status->text : "\n");
+		//smtp_enumerate_recipients(message, print_recipient_status, NULL);
 	}
 
-	if((sock = fdopen(sockfd, "w")) == NULL){
-		result->code = ACTION_ERROR;
-		snprintf(result->desc, RESULT_DESC_LEN, "fdopen error");
-		return;
-	}
+	/* Free resources consumed by the program.
+	 */
+	smtp_destroy_session(session);
+	auth_destroy_context(authctx);
 
-	if(gethostname(hostname, 256) == -1){
-		strncpy(hostname, "sysalarm", 256);
-	}
-
-	prepare_mail_message(config, buffer, MAIL_MSG_MAX_SIZE, cond_res);
-	fprintf(sock, "HELO %s\n", hostname);
-	fflush(sock);
-	fprintf(sock, "MAIL FROM: <%s>\r\n", config->mail_from);
-	fflush(sock);
-	fprintf(sock, "RCPT TO: <%s>\n", config->mail_to);
-	fflush(sock);
-	fprintf(sock, "DATA\n");
-	fflush(sock);
-	fprintf(sock, "%s\r\n.\r\n", buffer);
-	fprintf(sock, "QUIT\n");
-	fflush(sock);
-
-	fclose(sock);
 
 	result->code =  ACTION_OK;
 }
